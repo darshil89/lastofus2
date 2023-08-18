e434737585a020c483514402cb23e9411118b76c
darshil89 <darshilmahraur67@gmail.com>
2023-08-18 19:17:39 +0530

    ..

--- src/components/introPage/page.jsx
+++ src/components/introPage/page.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+"use client";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+import React from "react";
 
 const IntroPage = () => {
-
+  useEffect(() => {
+    axios.get("https://indnutrientsapi.tech/food?type=vegetarian").then((res) => {
+        const limitedData = res.slice(0, 10);
     
-  return (
-    <div>
-      Home Page
-    </div>
-  )
-}
+        // Now you have an array of limited data
+        console.log(limitedData);
+    });
+  }, []);
+  return <div>Home Page</div>;
+};
 
-export default IntroPage
+export default IntroPage;
