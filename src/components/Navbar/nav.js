8994cd357a1e2d1a7bb8ee7ba0dd4c11758890cc
darshil89 <darshilmahraur67@gmail.com>
2023-08-18 18:51:11 +0530

    css

--- src/components/Navbar/nav.js
+++ src/components/Navbar/nav.js
@@ -11,17 +11,17 @@ const Nav = () => {
       <ul className={styles.navbar_links}>
         <li>
           <Link href="/"className={styles.navbar_link}>
-           Home
-          </Link>
-        </li>
-        <li>
-          <Link href="/about" className={styles.navbar_link}>
            About
           </Link>
         </li>
         <li>
+          <Link href="/about" className={styles.navbar_link}>
+           Sign Up
+          </Link>
+        </li>
+        <li>
           <Link href="/contact" className={styles.navbar_link}>
-            Contact
+            Sign In
           </Link>
         </li>
       </ul>
