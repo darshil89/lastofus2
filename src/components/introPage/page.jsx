7e5ac64ed262f44f05262d109c1deb340a767767
darshil89 <darshilmahraur67@gmail.com>
2023-08-18 19:29:35 +0530

    ..

--- src/components/introPage/page.jsx
+++ src/components/introPage/page.jsx
@@ -2,18 +2,24 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-import React from "react";
-
 const IntroPage = () => {
   useEffect(() => {
-    axios.get("https://indnutrientsapi.tech/food?type=vegetarian").then((res) => {
-        const limitedData = res.slice(0, 10);
-    
-        // Now you have an array of limited data
-        console.log(limitedData);
-    });
+    axios
+      .get("https://indnutrientsapi.tech/food?type=vegetarian")
+
+      .then((response) => {
+        const data = response.data.item;
+        if (data) {
+          const limitedData = data.slice(0, 10);
+          console.log(limitedData);
+        } else {
+          console.error("API response data is undefined or empty.");
+        }
+      });
   }, []);
-  return <div>Home Page</div>;
+  return <>
+  
+  </>;
 };
 
 export default IntroPage;
